2d074f29c66614fd4b024889850eeb5dfd52a263
Daniel Spencer <dms305@hku.hk>
2020-05-07 22:06:12 +0800

    Change segment detection algorithm
    Line segment detection now uses FastLineDetector for improved performance. The probabilistic Hough Lines algorithm performed poorly, detecting shorter line segments than expected.
    
    Add tests for feature detection functionality, including the start of attempts to work with real molecules.

--- molrec/molecule_detection/image_utils.py
+++ molrec/molecule_detection/image_utils.py
@@ -6,20 +6,20 @@ This module provides utility functions for working with images.
 import cv2
 
 
-def annotate_image(img, corners=None, lines=None):
+def annotate_image(image, corners=None, lines=None):
     """
-    Annotates the provided `img` using circles for `corners` and lines for
+    Annotates the provided `image` using circles for `corners` and lines for
     `lines`.
 
     """
     if corners is not None:
         for i in corners:
             x, y = i.ravel()
-            cv2.circle(img, (x, y), 3, 255, -1)
+            cv2.circle(image, (x, y), 3, 255, -1)
 
     if lines is not None:
         for line in lines:
             for x1, y1, x2, y2 in line:
-                cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-    return img
+    return image
